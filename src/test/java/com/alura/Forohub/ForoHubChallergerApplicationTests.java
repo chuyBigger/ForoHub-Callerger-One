5999b841b557e83a319f6a4eb3ee95f475cc409b
Jesus Medina Casas <chuytec15@gmail.com>
2025-07-25 01:09:43 -0600

    Primer Commit

---
+++ src/test/java/com/alura/Forohub/ForoHubChallergerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alura.Forohub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForoHubChallergerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
